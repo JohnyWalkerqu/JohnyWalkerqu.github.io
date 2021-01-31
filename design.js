bd32c0b9843b631a6ff2a4d512b71577f8c2be93
JohnyWalkerqu <nicolas.walk@web.de>
2021-01-31 18:38:00 +0100

    Nav bar added

--- design.js
+++ design.js
@@ -1,4 +1,21 @@
+// NAV BAR DESIGN
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
+
 addEventListener = function () {
+
+
 
   var form = getElementById('contact-form');
 
