ad9c82606ff7978bd923e3a4ca0a5ef79159fb96
JohnyWalkerqu <nicolas.walk@web.de>
2021-02-05 14:28:39 +0100

    Completely revised the Responsive Layout and added dynamic style

--- design.js
+++ design.js
@@ -1,6 +1,11 @@
-// NAV BAR DESIGN
+var sectionLanding = document.querySelector('.landing');
+var sectionContact = document.querySelector('.input-form');
+
+
+// NAV BAR
 var menuBtn = document.querySelector('.menu-btn');
 var nav = document.querySelector('nav');
+var navDesktop = document.querySelector('.nav-desktop')
 var lineOne = document.querySelector('nav .menu-btn .line--1');
 var lineTwo = document.querySelector('nav .menu-btn .line--2');
 var lineThree = document.querySelector('nav .menu-btn .line--3');
@@ -12,6 +17,73 @@ menuBtn.addEventListener('click', () => {
     lineThree.classList.toggle('line-cross');
     link.classList.toggle('fade-in');
 })
+
+// show navbar on page scroll
+// calculate for mobile
+// var windowHeight = window.innerHeight,
+//     navBarHeight = nav.offsetHeight,
+//     windowNavOffset = windowHeight-navBarHeight;
+//
+// // calculate for desktop
+var windowHeight = window.innerHeight,
+    navBarDesktopHeight = navDesktop.offsetHeight,
+    windowNavDesktopOffset = windowHeight - navBarDesktopHeight;
+
+// document.addEventListener('scroll', function(e) {
+//   var scrollPosition = pageYOffset;
+//
+//   if (scrollPosition > windowNavOffset) {
+//     console.log('current scroll position' + scrollPosition);
+//     console.log('current window nav offset' + windowNavOffset);
+//     // adds the class .nav--scroll to the nav-element
+//     // menuBtn.classList.add('nav--scroll');
+//   }
+//   if (scrollPosition > windowNavDesktopOffset) {
+//     console.log('navBarDesktopHeight is' + navBarDesktopHeight);
+//     console.log('current scroll position' + scrollPosition);
+//     console.log('current window nav offset' + windowNavOffset);
+//     // console.log('scroll for desktop');
+//     // navDesktop.classList.add('nav--scroll-desktop');
+//   }
+//    else {
+//     // nav.classList.remove('nav--scroll');
+//     // navDesktop.classList.remove('nav--scroll-desktop');
+//   }
+// });
+
+document.addEventListener('scroll', function() {
+  var scrollPosition = pageYOffset;
+
+  if (scrollPosition > 50) {
+    console.log('class added: nav--scroll-desktop');
+    navDesktop.classList.add('nav--scroll-desktop');
+  }
+  else {
+    console.log('class removed: nav--scroll-desktop');
+    navDesktop.classList.remove('nav--scroll-desktop');
+  }
+})
+
+
+// SEARCHBAR
+// add search bar functions
+// JavaScript code
+function search_content() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('animals');
+
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";
+		}
+	}
+}
+
+
 
 addEventListener = function () {
 
