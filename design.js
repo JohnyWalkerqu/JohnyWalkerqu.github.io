50cd6c4ef59d64ef423a50be9efe3e9e7fdc7a90
JohnyWalkerqu <nicolas.walk@web.de>
2021-02-10 12:23:36 +0100

    layout changed + mobile menu fixes

--- design.js
+++ design.js
@@ -5,51 +5,37 @@ var sectionContact = document.querySelector('.input-form');
 // NAV BAR
 var menuBtn = document.querySelector('.menu-btn');
 var nav = document.querySelector('nav');
-var navDesktop = document.querySelector('.nav-desktop')
+var navDesktop = document.querySelector('.nav-desktop');
+var navMobile = document.querySelector('.nav-mobile');
 var lineOne = document.querySelector('nav .menu-btn .line--1');
 var lineTwo = document.querySelector('nav .menu-btn .line--2');
 var lineThree = document.querySelector('nav .menu-btn .line--3');
 var link = document.querySelector('nav .nav-links');
 menuBtn.addEventListener('click', () => {
+    navMobile.classList.add('nav-full-height');
     nav.classList.toggle('nav-open');
     lineOne.classList.toggle('line-cross');
     lineTwo.classList.toggle('line-fade-out');
     lineThree.classList.toggle('line-cross');
     link.classList.toggle('fade-in');
+
+    if (nav.classList.contains('nav-open')) {
+      console.log('Nav-Mobile is open');;
+    }
+    else {
+      console.log('Nav-Mobile is closed');
+      // function to remove class after the menu-bar is completely closed
+      setTimeout(function(){
+        console.log('Timeout fired');
+        navMobile.classList.remove('nav-full-height');
+      }, 800);
+    }
 })
 
-// show navbar on page scroll
-// calculate for mobile
-// var windowHeight = window.innerHeight,
-//     navBarHeight = nav.offsetHeight,
-//     windowNavOffset = windowHeight-navBarHeight;
-//
 // // calculate for desktop
 var windowHeight = window.innerHeight,
     navBarDesktopHeight = navDesktop.offsetHeight,
     windowNavDesktopOffset = windowHeight - navBarDesktopHeight;
-
-// document.addEventListener('scroll', function(e) {
-//   var scrollPosition = pageYOffset;
-//
-//   if (scrollPosition > windowNavOffset) {
-//     console.log('current scroll position' + scrollPosition);
-//     console.log('current window nav offset' + windowNavOffset);
-//     // adds the class .nav--scroll to the nav-element
-//     // menuBtn.classList.add('nav--scroll');
-//   }
-//   if (scrollPosition > windowNavDesktopOffset) {
-//     console.log('navBarDesktopHeight is' + navBarDesktopHeight);
-//     console.log('current scroll position' + scrollPosition);
-//     console.log('current window nav offset' + windowNavOffset);
-//     // console.log('scroll for desktop');
-//     // navDesktop.classList.add('nav--scroll-desktop');
-//   }
-//    else {
-//     // nav.classList.remove('nav--scroll');
-//     // navDesktop.classList.remove('nav--scroll-desktop');
-//   }
-// });
 
 document.addEventListener('scroll', function() {
   var scrollPosition = pageYOffset;
@@ -57,10 +43,12 @@ document.addEventListener('scroll', function() {
   if (scrollPosition > 50) {
     console.log('class added: nav--scroll-desktop');
     navDesktop.classList.add('nav--scroll-desktop');
+    navMobile.classList.add('nav--scroll-mobile');
   }
   else {
     console.log('class removed: nav--scroll-desktop');
     navDesktop.classList.remove('nav--scroll-desktop');
+    navMobile.classList.remove('nav--scroll-mobile');
   }
 })
 
@@ -84,23 +72,26 @@ function search_content() {
 }
 
 
+// CONTACT FORM
+var form = document.getElementById('contact-form');
+var submit = document.getElementById('submit');
 
-addEventListener = function () {
-
-
-
-  var form = getElementById('contact-form');
-
+submit.addEventListener('click', () => {
   // Submit form with id function.
   function submit_by_id() {
     var firstname = document.getElementById("firstname").value;
     var surname = document.getElementById("surname").value;
+    var adress = document.getElementById("adress").value;
     var email = document.getElementById("email").value;
-
+    console.log(firstname);
+    console.log(surname);
+    console.log(adress);
+    console.log(email);
     if (validation()) {// Calling validation function
       form.submit(); //form submission
       alert(" firstname : " + firstname +
             " n surname : " + surname +
+            " n zip-code : " + adress +
             " n Email : " + email +
             " n Form Id : " + form.getAttribute("id") + "nn Form Submitted Successfully......");
     }
@@ -110,6 +101,7 @@ addEventListener = function () {
   function validation() {
     var firstname = document.getElementById('firstname').value;
     var surname = document.getElementById('surname').value;
+    var email = document.getElementById('adress').value;
     var email = document.getElementById('email').value;
     // var marketing = document.getElementById('marketing').value;
     var emailReg = mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -126,9 +118,4 @@ addEventListener = function () {
       return true;
     }
   }
-
-  form.onclick = function() {
-    alert('hello');
-    console.log('hello');
-  }
-};
+});
